1a4a2badf4cb43a351d173b7f97365265c359bd9
omar8157 <mrm548500@gmail.com>
2026-02-23 01:09:43 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:store_app/presentation/home/home_view.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+     
+      home: const HomeView()
+    );
+  }
+}
+
